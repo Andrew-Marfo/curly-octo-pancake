fea64d4fe0c22fc923051b948a08680c91300e09
Andrew Marfo <andrewmarfo23@gmail.com>
2024-10-02 21:02:50 +0000

    search box

--- src/components/header-and-chat-window/header-component/header.component.jsx
+++ src/components/header-and-chat-window/header-component/header.component.jsx
@@ -7,17 +7,19 @@ const Header = () => {
     return (
         <div className="header-container">
             <h2 className="header-title">ICG chat</h2>
-            <div className="search-container">
-                <FaMagnifyingGlass size={20} color="white" />
-                <input type="text" className="header-search-box" placeholder="Search" />
+            <div className="search-buttons-and-profile-container">
+                <div className="search-container">
+                    <FaMagnifyingGlass size={15} color="white" />
+                    <input type="text" className="header-search-box" placeholder="Search" />
+                </div>
+                <div className="header-button">
+                    <MdOutlineSettings size={20} color="white" />
+                </div>
+                <div className="header-button">
+                    <MdOutlineNotifications size={20} color="white" />
+                </div>
+                <div className="user-profile" />
             </div>
-            <div className="header-button">
-                <MdOutlineSettings size={20} color="white" />
-            </div>
-            <div className="header-button">
-                <MdOutlineNotifications size={20} color="white" />
-            </div>
-            <div className="user-profile" />
         </div>
     );
 }
