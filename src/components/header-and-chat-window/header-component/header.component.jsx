1499980edd08ab445703163820136828514e2084
Andrew Marfo <andrewmarfo23@gmail.com>
2024-10-02 20:31:17 +0000

    icons

--- src/components/header-and-chat-window/header-component/header.component.jsx
+++ src/components/header-and-chat-window/header-component/header.component.jsx
@@ -1,11 +1,23 @@
+import { FaMagnifyingGlass } from "react-icons/fa6";
+import { MdOutlineSettings, MdOutlineNotifications } from "react-icons/md";
+
 const Header = () => {
     return (
         <div className="header-container">
             <h2 className="header-title">ICG chat</h2>
-            <div className="search-container"></div>
-            <div className="header-button"></div>
-            <div className="header-button"></div>
-            <div className="user-profile"></div>
+            <div className="search-container">
+                <FaMagnifyingGlass size={20} color="white" />
+                <input type="text" className="header-search-box" placeholder="Search" />
+            </div>
+            <div className="header-button">
+                <MdOutlineSettings size={20} color="white"/>
+            </div>
+            <div className="header-button">
+                <MdOutlineNotifications size={20} color="white"/>
+            </div>
+            <div className="user-profile">
+                
+            </div>
         </div>
     );
 }
