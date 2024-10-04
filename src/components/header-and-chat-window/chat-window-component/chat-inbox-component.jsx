92e866370c0322f46aaaac606cb10f62b0f9e506
Andrew Marfo <andrewmarfo23@gmail.com>
2024-10-04 21:40:23 +0000

    chat input finished

--- src/components/header-and-chat-window/chat-window-component/chat-inbox-component.jsx
+++ src/components/header-and-chat-window/chat-window-component/chat-inbox-component.jsx
@@ -1,14 +1,14 @@
 import { MdAttachFile } from "react-icons/md";
-import { MdSend } from "react-icons/md";
+import { MdOutlineSend } from "react-icons/md";
 import { MdInbox } from "react-icons/md";
 
 const ChatInbox = () => {
     return (
         <div className="chat-inbox-container">
-            <img src={MdAttachFile} alt="Attachment" />
-            <input type="Write a message..." />
-            <img src={MdInbox} alt="Inbox" />
-            <img src={MdSend} alt="Send" />
+            <MdAttachFile className="attach" size={20} color="white" />
+            <input type="text" placeholder="Write a message..." />
+            <MdInbox className="options" size={20} color="white" />
+            <MdOutlineSend className="send" size={20} color="white" />
         </div>
     );
 }
